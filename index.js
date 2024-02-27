d299a2f56cabaa6b48e4bd41a93b34b29304590c
Liv Kaminske <livkaminske@gmail.com>
2024-02-27 18:54:11 -0500

    fix buttons for real this time

---
+++ index.js
@@ -0,0 +1,19 @@
+function portfolio(){
+    location.href="./portfolios.html"
+}
+
+function booking(){
+    location.href="./booking.html"
+}
+
+function artists(){
+    location.href="./artists.html"
+}
+function awards(){
+    location.href="./awards.html"
+
+}
+function blog(){
+    location.href="./blog.html"
+
+}
